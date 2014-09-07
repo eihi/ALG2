9c4f34872b9d72648dfda8db708017cf1130ead4
Niek Willems <niek.willems91@gmail.com>
2014-09-07 13:33:23 +0200

    opdrachten week 1

--- ALG2-HW1/Program.cs
+++ ALG2-HW1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,14 +21,31 @@ namespace ALG2_HW1
 
             // Store N & K
             int n = Convert.ToInt32(nStr);
-            int k = Convert.ToInt32(kStr); 
+            int k = Convert.ToInt32(kStr);
 
-            Console.WriteLine("Opdracht B.1 (" + n + "/" + k + ") = " + NOverK1(n, k));
-            Console.WriteLine("Opdracht B.2 (" + n + "/" + k + ") = " + NOverK2(n, k));
-            Console.WriteLine("Opdracht B.3 (" + n + "/" + k + ") = " + NOverK3(n, k));
-            Console.WriteLine("Opdracht B.4 (" + n + "/" + k + ") = " + NOverK4(n, k));
+            Stopwatch s = new Stopwatch();
+            s.Start();
+            double nk1 = NOverK1(n, k);
+            s.Stop();
+            Console.WriteLine("Opdracht B.1 (" + n + "/" + k + ") = " + nk1 + " (" + s.Elapsed + "sec)");
+            s.Reset();
+            s.Start();
+            double nk2 = NOverK2(n, k);
+            s.Stop();
+            Console.WriteLine("Opdracht B.2 (" + n + "/" + k + ") = " + nk2 + " (" + s.Elapsed + "sec)");
+            s.Reset();
+            s.Start();
+            double nk3 = NOverK3(n, k);
+            s.Stop();
+            Console.WriteLine("Opdracht B.3 (" + n + "/" + k + ") = " + nk3 + " (" + s.Elapsed + "sec)");
+            s.Reset();
+            s.Start();
+            double nk4 = NOverK4(n, k);
+            s.Stop();
+            Console.WriteLine("Opdracht B.4 (" + n + "/" + k + ") = " + nk4 + " (" + s.Elapsed + "sec)");
             Console.Read();
         }
+
         private static double f(int n)
         {
             if (n > 0)
@@ -41,27 +59,44 @@ namespace ALG2_HW1
             }
             return 0;
         }
+
         private static double NOverK1(int n, int k)
         {   
             return f(n)/(f(k)*(f(n-k)));   
-        }               
+        } 
+              
         private static double NOverK2(int n, int k)
         {
             double result = 1;
-            for (int i = 0; i <= k ; i++)
+            for (int i = 1; i <= k; i++)
             {
-                result *= ((n - i) / (i+1));
+                result = result * (n-(k-i))/i;
             }
             return result;
-        }               
+        }   
+            
         private static double NOverK3(int n, int k)
         {
-            return ((n - 1) / (k - 1)) * n / k;
-        }               
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+            else
+            {
+                return NOverK3(n - 1, k - 1) * n / k;
+            }
+        }  
+             
         private static double NOverK4(int n, int k)
         {
-            double result = 0;
-            return result;
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+            else
+            {
+                return NOverK4(n - 1, k) + NOverK4(n - 1, k - 1);
+            }
         }
     }
 }
